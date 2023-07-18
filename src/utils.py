d20276c71c45ab06c01e20e454632df629c6d749
Chandan <chandankr014@gmail.com>
2023-07-19 00:36:02 +0530

    used dill module

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -58,7 +57,7 @@ def evaluate_model(xtrn,ytrn,xtst,ytst, models, params):
 def load_object(file_path):
    try:
        with open(file_path, "rb") as file_obj:
-           return pickle.load(file_obj)
+           return dill.load(file_obj)
        
    except Exception as e:
        raise CustomException(e,sys)
