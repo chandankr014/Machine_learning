90017f95fc07f94818b49c91a2ce75831ab5a419
Chandan <chandankr014@gmail.com>
2023-08-13 00:51:36 +0530

    harsh commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, render_template, session, make_response, jsonify
-import jwt
+from flask import Flask, request, render_template, make_response, jsonify
 import datetime
-from functools import wraps
 import json
+import requests
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 from src.pipeline.predict_pipeline import CustomData
 from src.pipeline.predict_pipeline import PredictionPipeline
@@ -14,7 +18,10 @@ warnings.filterwarnings("ignore", category=UserWarning)
 # warnings.filterwarnings("default", category=UserWarning)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "3d2a877fc3074ec59014b8ad8b912143"
+app.config['JWT_SECRET_KEY'] = "3d2a877fc3074ec59014b8ad8b912143"
+
+# creating jwt manager and giving app into it
+jwt = JWTManager(app)
 
 ## functions
 # storinng user credentials
@@ -39,7 +46,7 @@ def check_username(username):
     except Exception as e:
         print("An error occurred:", e)
         return None
-    
+
 
 ## routes for app 
 @app.route("/")
@@ -53,9 +60,8 @@ def get_credentials():
         with open('templates/credentials.json', 'r') as file:
             json_text = file.read()
             return json_text
-            
-    except:
-        pass
+    except Exception as e:
+        return jsonify({"Error":e })
     
 
 @app.route("/login", methods=['GET','POST'])
@@ -63,66 +69,39 @@ def login():
     if request.method=="POST":
         username = request.form.get('username')
         password = request.form.get('password')
-        if username and password != "admin":
-            session["logged_in"] = True
-
-            jwt_payload = {
-                'user':request.form.get('username'),
-                'exp': datetime.datetime.now() + datetime.timedelta(seconds=120)
-            }
-            token = jwt.encode(
-                jwt_payload,
-                app.config['SECRET_KEY']
-            )
-            
-            # storing user credentials in json 
+        if username == "admin":
+            access_token = create_access_token(identity=username)
+            # saving user credentials
             try:
-                # check if username is already there
-                if check_username(username) == None:
-                    store_credentials(username, password, token)
-            except:
-                return "couldn't save credentials"
-
-            return jsonify({ 'token': token })
+                store_credentials(username, password, access_token)
+            except Exception as e:
+                return jsonify({"Error":e })
+            
+            headers = {
+                "Authorization": f"Bearer {access_token}"
+            }
+            # response = requests.get("localhost:5000/auth", headers=headers)
+            print(jsonify(headers))
+            
+            return jsonify({"token ": access_token})
         
         else:
-            return make_response("Unable to Verify User", 403)
-        
+            return make_response("unauthorized", 403)
     else:
         return render_template("login.html")
 
-#
-# write other function for authorization
-#
-def token_required(func):
-    # decorator factory which invoks update_wrapper() method and passes decorated function as an argument
-    @wraps(func)
-    def decorated(*args, **kwargs):
-        token = request.args.get('token')
-        if not token:
-            return jsonify({'Alert!': 'Token is missing!'}), 401
-
-        try:
-            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        except:
-            return jsonify({'Message': 'Invalid token'}), 403
-        # You can use the JWT errors in exception
-        # except ExpiredSignatureError:
-        #     return "Token has expired", 401
-        # except DecodeError:
-        #     return 'Invalid token. Please log in again.', 401
-        return func(*args, **kwargs)
-    return decorated
-
 # public and auth
 
-@app.route('/auth')
-@token_required
+@app.route('/auth', methods=['GET', 'POST'])
+@jwt_required()
 def auth():
     print('JWT is verified. Welcome to your dashboard !')
+    # curr_user = get_jwt_identity()
+    # return jsonify(logged_in_as=curr_user), 200
     return render_template("model.html")
 
 @app.route('/public')
+# @jwt_required() #token req
 def public():
     return make_response('For Public. Go to /auth and verify your token', 200)
 
